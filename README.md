dcaff7d04ffdc985ae62a0af66dd092418baed98
victorbiscio <victorbiscio1@hotmail.com>
2022-12-13 12:53:49 +0000

    Adding details to README

--- README.md
+++ README.md
@@ -2,34 +2,26 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+The objective of this project was to develop a daily scheduler based on real-time hours.
+It was created with the intention of enhancing my abilities while utilising Moment and the jQuery libraries.
+The project employs colour coding to show how the scheduling relates to the current time and can store user data even when the browser is reloaded.
+The major reason I created this project, among other things, was to better grasp the jQuery syntax and how it differs from standard JS.
 
 ## Table of Contents (Optional)
 
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+N/A
+
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
+    ```The below image illustrates the color coding in action based on the user's current time-zone
+    ![alt text](/images/Screenshot.png)
     ```
 
 ## Credits
@@ -64,11 +56,7 @@ SOFTWARE.
 
 ![jQuery](https://img.shields.io/badge/jQuery-v.01-green)
 
-## Features
-
-If your project has a lot of features, list them here.
-
-
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+* Color coding based on real-time hours fully functional.
+* LocalStorage able to maintain and/or replace user's input even after the browser is fully reloaded. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,63 @@
-var today =  moment();
-var test = $('.timeblock')
-var hour = $('#hour');
-var currentDay = $('#currentDay').text(today.format("dddd, MMMM Do"));
-var successMsg = $('#saveMessage');
+var today = moment();
+var test = $(".timeblock");
+var hour = $("#hour");
+var currentDay = $("#currentDay").text(today.format("dddd, MMMM Do"));
+var successMsg = $("#saveMessage");
 savedTimer = 5;
 
 // Render localstorage values
-$('#input1').text(localStorage.getItem('9'));
-$('#input2').text(localStorage.getItem('10'));
-$('#input3').text(localStorage.getItem('11'));
-$('#input4').text(localStorage.getItem('12'));
-$('#input5').text(localStorage.getItem('13'));
-$('#input6').text(localStorage.getItem('14'));
-$('#input7').text(localStorage.getItem('15'));
-$('#input8').text(localStorage.getItem('16'));
-$('#input9').text(localStorage.getItem('17'));
+$("#input1").text(localStorage.getItem("9"));
+$("#input2").text(localStorage.getItem("10"));
+$("#input3").text(localStorage.getItem("11"));
+$("#input4").text(localStorage.getItem("12"));
+$("#input5").text(localStorage.getItem("13"));
+$("#input6").text(localStorage.getItem("14"));
+$("#input7").text(localStorage.getItem("15"));
+$("#input8").text(localStorage.getItem("16"));
+$("#input9").text(localStorage.getItem("17"));
 
-$( ".saveBtn" ).on('click', function( event ) {
-  successMsg.text("Appointment saved to localStorage")
+$(".saveBtn").on("click", function (event) {
+  successMsg.text("Appointment saved to localStorage");
   event.preventDefault();
   return;
 });
 
-// Button function 
-$( ".saveBtn" ).on( "click", saveValue);
-function saveValue(){
-    var id = this.parentElement.parentElement.id; // get the sender's id to save it . 
-    var val = this.parentElement.parentElement.children[1].children[0].value; // get the value. 
-    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
-    savedTimer = 5;
-    
-    var hideSaved = setInterval(function () {
-      savedTimer--;
-          console.log(savedTimer);
-          if (savedTimer <= 0) {
-            clearInterval(hideSaved);
-            successMsg.css('display', 'none') 
-          }
-    }, 1000);
-    var showSaved = setInterval(function () {
-      successMsg.css('display', 'flex') 
-      clearInterval(showSaved);
-    }, 1000);
-  }
-
-
-
-
+// Button function
+$(".saveBtn").on("click", saveValue);
+function saveValue() {
+  var id = this.parentElement.parentElement.id; // Grab user's id in order to store it 
+  var val = this.parentElement.parentElement.children[1].children[0].value; // Gets the value of user's input
+  localStorage.setItem(id, val); // This command will overwrite the user's input 
+  savedTimer = 5;
+// Save message logic
+  var hideSaved = setInterval(function () {
+    savedTimer--;
+    console.log(savedTimer);
+    if (savedTimer <= 0) {
+      clearInterval(hideSaved);
+      successMsg.css("display", "none");
+    }
+  }, 1000);
+  var showSaved = setInterval(function () {
+    successMsg.css("display", "flex");
+    clearInterval(showSaved);
+  }, 1000);
+}
 
 arrayOfHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-
+// Defining color coding for rows
 function init() {
-  arrayOfHours.forEach(function(el) {
-  var rowEl = $('#' + el);
-  if(el == today.hour()){
-    rowEl.children().eq(1).addClass('present'); 
-  } else if(el < today.hour()){
-    rowEl.children().eq(1).addClass('past');
-  } else if(el > today.hour()){
-    rowEl.children().eq(1).addClass('future');
-  }
-});
+  arrayOfHours.forEach(function (el) {
+    var rowEl = $("#" + el);
+    if (el == today.hour()) {
+      rowEl.children().eq(1).addClass("present");
+    } else if (el < today.hour()) {
+      rowEl.children().eq(1).addClass("past");
+    } else if (el > today.hour()) {
+      rowEl.children().eq(1).addClass("future");
+    }
+  });
 }
 
 init();
-
-
-
-
-
-
-
-  ///Create Timeslots
-  //create array with the hours
-  //loop the array of hours to create the rows
-        
-    //create TAG for row/hour/input/button 
-
-    //give class to row/hour/input/button 
-
-    //create a dataset to keep the hour in the (row)
-
-    //style timeslots based on the hour comparing to "today.hour()"
-
-  
-      //append all tags
-  
-    // add event listener to add local storage when the save button is clicked in specific timeblock.(Data key "time")
